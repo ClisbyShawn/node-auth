bf8d66184a6b5320d4321cf8ede701c4adbbf53e
Shawn Clisby <shawn.clisby@gmail.com>
2020-10-29 23:22:46 -0400

    Joi library added, request validation added for registering a User, hash and salt password.

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,4 +24,19 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+function validate(user) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net", "edu", "org"] },
+      })
+      .required(),
+    password: Joi.string().min(5).max(12).required(),
+  });
+  return schema.validate(user);
+}
+
 module.exports.User = User;
+module.exports.validate = validate;
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
-const { User } = require("../models/User");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User, validate } = require("../models/User");
 
 //Register New User
+router.post("/register", async (req, res) => {
+  const user = req.body;
+  const { error } = validate(user);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //encrypt password
+  const salt = await bcrypt.genSalt(12);
+  const password = await bcrypt.hash(user.password, salt);
+
+  const newUser = await new User({
+    name: user.name,
+    email: user.email,
+    password,
+  }).save();
+
+  res.send(_.pick(newUser, ["_id", "name", "email", "meta_data"]));
+});
 
 //Login User
 
