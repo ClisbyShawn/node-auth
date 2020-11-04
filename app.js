691a0f22a626880ad80a5a0774d165f1d325ee4e
Shawn Clisby <shawn.clisby@gmail.com>
2020-11-04 16:04:50 -0500

    CORS policy, allowing access to be shared from the given origin, all for now.

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ require("./services/mongoose");
 const auth = require("./routes/auth");
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use("/auth", auth);
 
 const PORT = process.env.PORT || 5000;
